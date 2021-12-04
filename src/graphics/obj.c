7e7b3e30bbb667544b450a0259d21b237d89cd68
Elias Sebastian Selenius Haneborg <ESSH@LIVE.NO>
2021-12-05 00:12:14 +0100

    obj parser progress

--- src/graphics/obj.c
+++ src/graphics/obj.c
@@ -6,7 +6,13 @@
 #include "../String.h"
 #include "../List.h"
 
+typedef struct {
+    u32 pos_index, normal_index, uv_index;
+} vertex_index;
 
+typedef struct {
+    vertex_index vertices[3];
+} face;
 
 void objLoad(const char* filename, MeshData* out_data) {
     FILE* file = fopen(filename, "r");
@@ -16,39 +22,86 @@ void objLoad(const char* filename, MeshData* out_data) {
         perror("objLoad() error");
     }
 
-    vec3* vertex_positions = listCreate(vec3);
+    vec3* positions = listCreate(vec3);
+    vec3* normals = listCreate(vec3);
+    face* faces = listCreate(face);
 
     char line[256];
     while (fgets(line, sizeof(line), file)) {
         if (line[0] == '#') continue;
 
         char* cursor = line;
-        if ((cursor = stringStartsWith(cursor, "v "))) {
-
+        if ((cursor = stringStartsWith(line, "v "))) {
+            
             vec3 v;
             v.x = strtof(cursor, &cursor);
             v.y = strtof(cursor, &cursor);
             v.z = strtof(cursor, &cursor);
             
-            listAdd(vertex_positions, v);
-        } /*else if ((cursor = stringStartsWith(cursor, "vt "))) {
+            listAdd(positions, v);
 
-        } else if ((cursor = stringStartsWith(cursor, "vn "))) {
+        } else if ((cursor = stringStartsWith(line, "vt "))) {
 
-        } else if ((cursor = stringStartsWith(cursor, "f "))) {
+        } else if ((cursor = stringStartsWith(line, "vn "))) {
+            
+            vec3 v;
+            v.x = strtof(cursor, &cursor);
+            v.y = strtof(cursor, &cursor);
+            v.z = strtof(cursor, &cursor);
+            
+            listAdd(normals, v);
 
-        }*/
+        } else if ((cursor = stringStartsWith(line, "f "))) {
+            face tri;
+
+            for (int i = 0; i < 3; i++) {
+                
+                tri.vertices[i].pos_index = strtol(cursor, &cursor, 10) - 1;
+                cursor++;
+
+                tri.vertices[i].uv_index = strtol(cursor, &cursor, 10) - 1;
+                cursor++;
+
+                tri.vertices[i].normal_index = strtol(cursor, &cursor, 10) - 1;
+            }
+
+            listAdd(faces, tri);
+        }
 
 
-        printf("%s", line);
+        //printf("%s", line);
     }
 
     fclose(file);
 
 
-    for (int i = 0; i < listLength(vertex_positions); i++) {
-        printf("[%i] = %f, %f, %f\n", i, vertex_positions[i].x, vertex_positions[i].y, vertex_positions[i].z);
+    /*for (int i = 0; i < listLength(positions); i++) {
+        printf("[%i] = %f, %f, %f\n", i, positions[i].x, positions[i].y, positions[i].z);
+    }
+    for (int i = 0; i < listLength(normals); i++) {
+        printf("[%i] = %f, %f, %f\n", i, normals[i].x, normals[i].y, normals[i].z);
     }
 
-    listDelete(vertex_positions);
+    for (int i = 0; i < listLength(faces); i++) {
+        printf("[%i] = ", i);
+        for (int j = 0; j < 3; j++) {
+            printf("%i, %i, %i      ", faces[i].vertices[j].pos_index, faces[i].vertices[j].uv_index, faces[i].vertices[j].normal_index);
+        }
+        printf("\n");
+    }*/
+
+    // TODO: make smooth shading by combining vertices into one, based on angle between normals 
+    // basic flat shading algo:
+    u32 len = out_data->vertexCount = out_data->indexCount = listLength(faces) * 3;
+    out_data->vertices = malloc(sizeof(vertex) * len);
+    out_data->indices = malloc(sizeof(u32) * len);
+    for (int i = 0; i < len; i++) {
+        out_data->indices[i] = i;
+        out_data->vertices[i].pos = faces[i / 3].vertices[]
+    }
+
+
+    listDelete(positions);
+    listDelete(normals);
+    listDelete(faces);
 }
