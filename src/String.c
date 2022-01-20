895ae4745a9c5a7fb3e03cc874557ba9a8fbb79b
Elias Sebastian Selenius Haneborg <ESSH@LIVE.NO>
2022-01-20 22:33:33 +0100

    string stuff

--- src/String.c
+++ src/String.c
@@ -1,6 +1,6 @@
 #include "String.h"
 #include <string.h>
-
+#include "List.h"
 
 Strview svFrom(char* str) {
     return (Strview) {
@@ -37,7 +37,62 @@ Strview svTrimEnd(Strview sv) {
     return sv;
 }
 
+Strview svTrimChar(Strview sv, char c) {
+    return svTrimCharStart(svTrimCharEnd(sv, c), c);
+}
 
+Strview svTrimCharStart(Strview sv, char c) {
+    while (*sv.data == c) {
+        sv.data++;
+        sv.length--;
+    }
+    return sv;
+}
+
+Strview svTrimCharEnd(Strview sv, char c) {
+    char* end = sv.data + sv.length;
+    while ((*--end) == c);
+    sv.length = end - sv.data + 1;
+    return sv;
+}
+
+
+b8 svStartsWith(Strview sv, char* start) {
+    u32 i = 0;
+    while (true) {
+        if (start[i] == '\0') return true;
+        if (sv.data[i] == '\0') return false;
+        if (start[i] != sv.data[i]) return false;
+        i++;
+    }
+}
+
+void sbInit(StringBuilder* sb) {
+    sb->length = 0;
+    sb->sections = listCreate(Strview);    
+}
+
+void sbDestroy(StringBuilder* sb) {
+    listDelete(sb->sections);
+}
+
+void sbAppend(StringBuilder* sb, Strview str) {
+    sb->length += str.length;
+    listAdd(sb->sections, str);
+}
+
+void sbCopyIntoBuffer(StringBuilder* sb, char* buffer, u32 bufferLength) {
+    u32 sectionsCount = listLength(sb->sections);
+    u32 index = 0;
+    for (u32 i = 0; i < sectionsCount; i++) {
+        Strview sv = sb->sections[i];
+        for (u32 j = 0; j < sv.length; j++) {
+            if (index >= bufferLength) return;
+            buffer[index] = sv.data[j];
+            index++;
+        }
+    }
+}
 
 
 
--- src/String.h
+++ src/String.h
@@ -2,18 +2,35 @@
 
 #include "types.h"
 
-typedef struct {
+typedef struct Strview {
     u32 length;
     char* data;
 } Strview;
 
-
 Strview svFrom(char* str);
 void svCopyTo(Strview src, char* dest);
+
 Strview svTrim(Strview sv);
 Strview svTrimStart(Strview sv);
 Strview svTrimEnd(Strview sv);
 
+Strview svTrimChar(Strview sv, char c);
+Strview svTrimCharStart(Strview sv, char c);
+Strview svTrimCharEnd(Strview sv, char c);
+
+b8 svStartsWith(Strview sv, char* start);
+
+
+
+typedef struct StringBuilder {
+    u32 length;
+    Strview* sections;
+} StringBuilder;
+
+void sbInit(StringBuilder* sb);
+void sbDestroy(StringBuilder* sb);
+void sbAppend(StringBuilder* sb, Strview str);
+void sbCopyIntoBuffer(StringBuilder* sb, char* buffer, u32 bufferLength);
 
 
 
