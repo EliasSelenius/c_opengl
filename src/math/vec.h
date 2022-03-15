a164841255d40f0b79f3c26e1ab7006409ced28f
Elias Sebastian Selenius Haneborg <ESSH@LIVE.NO>
2022-03-15 02:38:30 +0100

    better vec funcs

--- src/math/vec.h
+++ src/math/vec.h
@@ -3,6 +3,8 @@
 #include "../types.h"
 #include "structs.h"
 
+#include <math.h>
+
 f32 vec2Dot(vec2 a, vec2 b);
 void vec2Normalize(vec2* v);
 
@@ -21,3 +23,11 @@ f32 vec4Dot(vec4* a, vec4* b);
 //f32 vec4Length(vec4 v) { return v.x*v.x + v.y*v.y + v.z*v.z + v.w*v.w; }
 
 //vec2 vec2(f32 x, f32 y) { return {}}
+
+
+inline f32 v3dot(vec3 a, vec3 b) { return a.x * b.x + a.y * b.y + a.z * b.z; }
+inline vec3 v3add(vec3 a, vec3 b) { return (vec3) { a.x + b.x, a.y + b.y, a.z + b.z }; }
+inline vec3 v3sub(vec3 a, vec3 b) { return (vec3) { a.x - b.x, a.y - b.y, a.z - b.z }; }
+inline vec3 v3mul(vec3 a, vec3 b) { return (vec3) { a.x * b.x, a.y * b.y, a.z * b.z }; }
+inline vec3 v3scale(vec3 a, f32 s) { return (vec3) { a.x * s, a.y * s, a.z * s }; }
+inline f32 v3length(vec3 a) { return sqrtf(a.x * a.x + a.y * a.y + a.z * a.z); }
